7c42e2daa1e155f2d0651f764f5e19533549ccd7
Stéphane <stephane.daviet@gmail.com>
2013-04-06 21:44:17 +0200

    Initialisation du dépôt 	new file:   _define.php 	new file:   style.css 	new file:   tpl/404.html 	new file:   tpl/_flv_player.html 	new file:   tpl/_footer.html 	new file:   tpl/_head.html 	new file:   tpl/_mp3_player.html 	new file:   tpl/_top.html 	new file:   tpl/archive.html 	new file:   tpl/archive_month.html 	new file:   tpl/atom-comments.xml 	new file:   tpl/atom.xml 	new file:   tpl/category.html 	new file:   tpl/home.html 	new file:   tpl/password-form.html 	new file:   tpl/post.html 	new file:   tpl/rss2-comments.xml 	new file:   tpl/rss2.xml 	new file:   tpl/rss2.xsl 	new file:   tpl/search.html 	new file:   tpl/user_head.html

---
+++ _define.php
@@ -0,0 +1,10 @@
+<?php
+if (!defined('DC_RC_PATH')) { return; }
+ 
+$this->registerModule(
+        /* Name */                      "Custom Theme",
+        /* Description*/                "Custom theme for Dotclear",
+        /* Author */                    "Stéphane & Hélène",
+        /* Version */                   '1'
+);
+?>
